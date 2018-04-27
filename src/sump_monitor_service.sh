f0b2369c49548d4feb1a4ee0afb18581799094e2
canthonybailey <c.anthony.bailey@gmail.com>
2018-04-27 17:43:41 -0400

    corrected reference to include src directory

--- src/sump_monitor_service.sh
+++ src/sump_monitor_service.sh
@@ -11,7 +11,7 @@
 ### END INIT INFO
  
 # Change the next 3 lines to suit where you install your script and what you want to call it
-DIR=/home/pi/sump
+DIR=/home/pi/sump_pump_monitor/src
 DAEMON=$DIR/sump_monitor_service.py
 DAEMON_NAME=sump_monitor_service
  
