af2fcf180fd56c9319c8af6ea383e84339d48cca
Tony Bailey <c.anthony.bailey@gmail.com>
2020-01-28 21:10:09 -0500

    Check and reconnect before publish

--- src/AWSIoTServices.py
+++ src/AWSIoTServices.py
@@ -5,19 +5,20 @@ import json
 
 logger = logging.getLogger("sump.AWSIoTService")
 
-myAWSIoTMQTTClient = None
-clientConnected = False
-DISCONNECT_COUNT = 0
+AWSIOT_CLIENT = None
+AWSIOT_CLIENT_CONNECTED = False
+AWSIOT_DISCONNECT_COUNT = 0
+AWSIOT_ERROR_COUNT = 0
 
 def onClientConnected():
     logger.debug("client online")
-    global clientConnected 
-    clientConnected = True
+    global AWSIOT_CLIENT_CONNECTED 
+    AWSIOT_CLIENT_CONNECTED = True
 
 def onClientDisconnected():
-    global clientConnected, DISCONNECT_COUNT
-    clientConnected = False
-    DISCONNECT_COUNT += 1
+    global AWSIOT_CLIENT_CONNECTED, DISCONNECT_COUNT
+    AWSIOT_CLIENT_CONNECTED = False
+    AWSIOT_DISCONNECT_COUNT += 1
 
     logger.debug("client offline {} times".format(DISCONNECT_COUNT))
 
@@ -30,60 +31,72 @@ def setupAWSClient(config_json):
     logger.debug("setting up device connection to AWS")
 
     # Init AWSIoTMQTTClient
-    global myAWSIoTMQTTClient
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(config_json["awsIoT"]["clientId"])
-    myAWSIoTMQTTClient.configureEndpoint(config_json["awsIoT"]["endpoint"], config_json["awsIoT"]["port"])
-    myAWSIoTMQTTClient.configureCredentials(
+    global AWSIOT_CLIENT
+    AWSIOT_CLIENT = AWSIoTMQTTClient(config_json["awsIoT"]["clientId"])
+    AWSIOT_CLIENT.configureEndpoint(config_json["awsIoT"]["endpoint"], config_json["awsIoT"]["port"])
+    AWSIOT_CLIENT.configureCredentials(
         config_json["awsIoT"]["rootCACertFile"], 
         config_json["awsIoT"]["privateKeyFile"], 
         config_json["awsIoT"]["certificateFile"])
 
     # AWSIoTMQTTClient connection configuration
-    myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+    AWSIOT_CLIENT.configureAutoReconnectBackoffTime(1, 32, 20)
     # Infinite offline Publish queueing
-    myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)
-    myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-    myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-    myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+    AWSIOT_CLIENT.configureOfflinePublishQueueing(-1)
+    AWSIOT_CLIENT.configureDrainingFrequency(2)  # Draining: 2 Hz
+    AWSIOT_CLIENT.configureConnectDisconnectTimeout(10)  # 10 sec
+    AWSIOT_CLIENT.configureMQTTOperationTimeout(5)  # 5 sec
 
     # track connection status
-    myAWSIoTMQTTClient.onOnline = onClientConnected
-    myAWSIoTMQTTClient.onOffline = onClientDisconnected
+    AWSIOT_CLIENT.onOnline = onClientConnected
+    AWSIOT_CLIENT.onOffline = onClientDisconnected
 
 
 def sendMessage(topic, message):
     logger.info("sending message to AWS")
 
     # Connect and subscribe to AWS IoT
-    global myAWSIoTMQTTClient
-    global clientConnected
-    global DISCONNECT_COUNT
+    global AWSIOT_CLIENT
+    global AWSIOT_CLIENT_CONNECTED
+    global AWSIOT_DISCONNECT_COUNT
+    global AWSIOT_ERROR_COUNT
 
     # add disconnect count to message
     messageJson = json.loads(message)
-    messageJson["disconnectCount"] = DISCONNECT_COUNT
+    messageJson["disconnectCount"] = AWSIOT_DISCONNECT_COUNT
+    messageJson["errorCount"] = AWSIOT_ERROR_COUNT
 
     try:
-       myAWSIoTMQTTClient.publish(topic, json.dumps(messageJson), 1)
+        if not AWSIOT_CLIENT_CONNECTED:
+            connect()
+
+        AWSIOT_CLIENT.publish(topic, json.dumps(messageJson), 1)
     except Exception as e:
-       logger.error("Could not publish message to topic {} : {}".format(topic, e))
+        AWSIOT_ERROR_COUNT +=1
+        logger.error("Could not publish message to topic {} : {}".format(topic, e))
 
 
 def listenForMessages(topic):
     # Connect and subscribe to AWS IoT
-    global myAWSIoTMQTTClient
+    global AWSIOT_CLIENT, AWSIOT_ERROR_COUNT
     try:
-        myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+        if not AWSIOT_CLIENT_CONNECTED:
+            connect()
+
+        AWSIOT_CLIENT.subscribe(topic, 1, customCallback)
         logger.info("listening for messages from AWS on topic {}".format(topic))
     except Exception as e:
-        logger.error("Could not subscribe to topic {}".format(topic))
+        AWSIOT_ERROR_COUNT +=1
+        logger.error("Could not subscribe to topic {}: {}".format(topic, e))
 
 def connect():
     # Connect and subscribe to AWS IoT
-    global myAWSIoTMQTTClient
+    global AWSIOT_CLIENT, AWSIOT_ERROR_COUNT
     try:
-        myAWSIoTMQTTClient.connect()
+        AWSIOT_CLIENT.connect()
     except Exception as e:
+        AWSIOT_ERROR_COUNT +=1
+        AWSIOT_CLIENT.disconnect()
         logger.error("Could not connect to AWS IoT: {}".format(e))
 
     
